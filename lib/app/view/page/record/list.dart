368bbd5d42c61f997f34b628143107f9f9f8e45d
u9uru <u9uru@icloud.com>
2021-03-07 17:46:48 +0900

    [update] record list page

--- lib/app/view/page/record/list.dart
+++ lib/app/view/page/record/list.dart
@@ -4,33 +4,62 @@ import 'package:omelet/app/config/export/default.dart';
 class RecordListView extends StatelessWidget {
   const RecordListView({Key key}) : super(key: key);
 
+  Widget getAppBar({bool searchMode = false}) {
+    return searchMode
+        ? AppBar(
+            foregroundColor: Colors.white,
+            backgroundColor: Colors.blueGrey,
+            title: Flexible(
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'search',
+                ),
+              ),
+            ),
+            actions: [
+              IconButton(
+                icon: Icon(Icons.close),
+                onPressed: () {},
+              ),
+            ],
+          )
+        : AppBar(
+            title: Text('メモ一覧'),
+            actions: [
+              IconButton(
+                icon: Icon(Icons.search),
+                onPressed: () {},
+              ),
+            ],
+          );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'メモ一覧',
-          textAlign: TextAlign.center,
-        ),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.add),
-            onPressed: () {},
-          ),
-        ],
-      ),
-      body: Container(
-        padding: EdgeInsets.all(16),
-        child: ListView.builder(
-          itemBuilder: (_, int index) {
-            return Card(
-              child: ListTile(
-                title: Text('title'),
-                subtitle: Text('2021/03/07'),
+    return SafeArea(
+      child: Scaffold(
+        appBar: getAppBar(searchMode: false),
+        floatingActionButton:
+            FloatingActionButton(child: Icon(Icons.add), onPressed: () {}),
+        floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+        body: Column(
+          children: [
+            Flexible(
+              child: Scrollbar(
+                child: ListView.builder(
+                  itemBuilder: (_, int index) {
+                    return Card(
+                      child: ListTile(
+                        title: Text('title'),
+                        subtitle: Text('2021/03/07'),
+                      ),
+                    );
+                  },
+                  itemCount: 20,
+                ),
               ),
-            );
-          },
-          itemCount: 2,
+            ),
+          ],
         ),
       ),
     );
