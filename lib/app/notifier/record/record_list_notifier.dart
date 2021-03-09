f9101b563ffecf879760e066551d04fe9d13a49a
u9uru <u9uru@icloud.com>
2021-03-09 20:59:05 +0900

    [refactor] 変数名変更

--- lib/app/notifier/record/record_list_notifier.dart
+++ lib/app/notifier/record/record_list_notifier.dart
@@ -11,25 +11,24 @@ class RecordListNotifier extends ChangeNotifier {
   // 表示するレコード
   Future<List<Record>> _records;
 
-  // 全件レコード
-  Future<List<Record>> _simpleRecords;
-
-  RecordListNotifier(this._ctx) {
-    _loadRecords();
-  }
+  // レコードのキャッシュ
+  Future<List<Record>> _cache;
 
   Future<List<Record>> get records => _records;
 
   RecordRepository get recordRepository => _ctx.read<RecordRepositorySembast>();
 
+  RecordListNotifier(this._ctx) {
+    _loadRecords();
+  }
+
   void _loadRecords() async {
-    _simpleRecords = recordRepository.findAll();
-    _records = _simpleRecords;
+    _records = _cache = recordRepository.findAll();
     notifyListeners();
   }
 
   void search(String query) async {
-    _records = _simpleRecords.then((value) {
+    _records = _cache.then((value) {
       return value.where((element) => element.title.contains(query)).toList();
     });
     notifyListeners();
