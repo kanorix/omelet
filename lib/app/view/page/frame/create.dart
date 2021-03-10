c2aef43eccde3353275834e48fa9a4e2b67bfd30
Shino <kenshin200057@gmail.com>
2021-03-10 16:33:59 +0900

    [create] テンプレート画面の作成(仮)

--- lib/app/view/page/frame/create.dart
+++ lib/app/view/page/frame/create.dart
@@ -1,4 +1,5 @@
 import 'package:omelet/app/config/export/default.dart';
+import 'package:omelet/app/config/export/model.dart';
 import 'package:omelet/app/notifier/record/record_list_notifier.dart';
 
 class FrameCreatePage extends StatelessWidget {
@@ -22,7 +23,78 @@ class FrameCreateView extends StatelessWidget {
       appBar: AppBar(
         title: Text('Frame作成'),
       ),
-      body: Placeholder(),
+      // 追加ボタン
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.save),
+        onPressed: () {},
+      ),
+      // 追加ボタンの位置指定
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(8),
+            child: TextFormField(
+              maxLength: 20,
+              decoration: InputDecoration(
+                labelText: 'テンプレート名',
+                border: OutlineInputBorder(),
+              ),
+            ),
+          ),
+          Card(
+            child: Container(
+              padding: EdgeInsets.all(10),
+              child: Column(children: [
+                TextFormField(
+                  maxLength: 20,
+                  decoration: InputDecoration(
+                    labelText: '内容1',
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xFF6200EE)),
+                    ),
+                  ),
+                ),
+                OutlinedButton(
+                  onPressed: () {
+                    // Respond to button press
+                  },
+                  child: Text("DATE"),
+                ),
+              ]),
+            ),
+          ),
+          Card(
+            child: Container(
+              padding: EdgeInsets.all(10),
+              child: Column(children: [
+                TextFormField(
+                  maxLength: 20,
+                  decoration: InputDecoration(
+                    labelText: '内容2',
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xFF6200EE)),
+                    ),
+                  ),
+                ),
+                OutlinedButton(
+                  onPressed: () {
+                    // Respond to button press
+                  },
+                  child: Text("DATE"),
+                ),
+              ]),
+            ),
+          ),
+          ElevatedButton.icon(
+            onPressed: () {
+              // Respond to button press
+            },
+            icon: Icon(Icons.add, size: 18),
+            label: Text("追加"),
+          )
+        ],
+      ),
     );
   }
 }
