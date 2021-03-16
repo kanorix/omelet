1b9815f40b67f7ad8f9ea3a510105a6968000929
u9uru <u9uru@icloud.com>
2021-03-17 01:42:07 +0900

    [update] notifier and page

--- lib/app/notifier/template/template_create_notifier.dart
+++ lib/app/notifier/template/template_create_notifier.dart
@@ -1,15 +1,19 @@
 import 'package:omelet/app/config/export/default.dart';
 import 'package:omelet/app/config/export/model.dart';
+import 'package:omelet/app/config/export/repository.dart';
+import 'package:omelet/common/mixin/validator_mixin.dart';
 
-class TemplateCreateNotifier extends ChangeNotifier {
+class TemplateCreateNotifier extends ChangeNotifier with ValidatorMixin {
   final Template _template;
 
   Template get template => _template;
+
   List<TemplateItem> get items => _template.items;
-  int get length => _template.items.length;
+
+  TemplateRepository get templateRepository => GetIt.I<TemplateRepository>();
 
   TemplateCreateNotifier({Template template})
-      : this._template = template ?? Template();
+      : this._template = template ?? Template(items: [TemplateItem()]);
 
   void addItem() {
     _template.items.add(TemplateItem());
@@ -17,6 +21,7 @@ class TemplateCreateNotifier extends ChangeNotifier {
   }
 
   void removeItem(int index) {
+    if (items.length == 1) return;
     _template.items.removeAt(index);
     notifyListeners();
   }
@@ -27,5 +32,13 @@ class TemplateCreateNotifier extends ChangeNotifier {
 
   void updateItemName(int index, String name) {
     _template.items[index].name = name;
+  }
+
+  Future<bool> save() async {
+    if (hasError()) return false;
+    // await Future.delayed(Duration(seconds: 1));
+    await templateRepository.insert(_template);
+    print(_template);
+    return true;
   }
 }
--- lib/app/view/page/template/create.dart
+++ lib/app/view/page/template/create.dart
@@ -1,6 +1,9 @@
 import 'package:omelet/app/config/export/default.dart';
 import 'package:omelet/app/config/export/model.dart';
 import 'package:omelet/app/notifier/template/template_create_notifier.dart';
+import 'package:omelet/app/view/component/dismissible_card.dart';
+import 'package:omelet/app/view/component/future_modal.dart';
+import 'package:omelet/common/func/validator.dart';
 
 class TemplateCreatePage extends StatelessWidget {
   const TemplateCreatePage({Key key}) : super(key: key);
@@ -28,84 +31,93 @@ class TemplateCreateView extends StatelessWidget {
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.save),
-        onPressed: () {},
+        onPressed: () {
+          futureModal(
+            context: context,
+            future: event.save(),
+            child: Text('Saving...'),
+          ).then((result) {
+            if (result) PageNavigator.of(context).pop();
+          });
+        },
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
       body: Scrollbar(
         child: SingleChildScrollView(
           padding: EdgeInsets.all(10),
-          child: Column(
-            children: [
-              Container(
-                padding: EdgeInsets.all(8),
-                child: TextFormField(
-                  maxLength: 20,
-                  decoration: InputDecoration(
-                    labelText: 'テンプレート名',
-                    border: OutlineInputBorder(),
+          child: Form(
+            key: event.formKey,
+            child: Column(
+              children: [
+                Container(
+                  padding: EdgeInsets.all(8),
+                  child: TextFormField(
+                    validator: emptyValidator,
+                    maxLength: 20,
+                    decoration: InputDecoration(
+                      labelText: 'テンプレート名',
+                      border: OutlineInputBorder(),
+                    ),
                   ),
                 ),
-              ),
-              ListView.builder(
-                scrollDirection: Axis.vertical,
-                shrinkWrap: true,
-                itemCount: state.length,
-                itemBuilder: (_, int index) {
-                  return Dismissible(
-                    key: UniqueKey(),
-                    child: Card(
-                      elevation: 6,
-                      child: Container(
-                        padding: EdgeInsets.all(10),
-                        child: Column(
-                          children: [
-                            TextFormField(
-                              maxLength: 20,
-                              decoration: InputDecoration(labelText: '表示名'),
-                              initialValue: state.items[index].name,
-                              onChanged: (v) {
-                                event.items[index].name = v;
-                              },
-                            ),
-                            DropdownButtonFormField<RecordType>(
-                              decoration: InputDecoration(labelText: 'レコードタイプ'),
-                              icon: Icon(Icons.arrow_downward),
-                              iconSize: 20,
-                              value: state.items[index].type,
-                              onChanged: (v) {
-                                event.updateItemType(index, v);
-                              },
-                              items: RecordType.values
-                                  .where((e) => e != RecordType.NONE)
-                                  .map<DropdownMenuItem<RecordType>>(
-                                      (RecordType type) {
-                                return DropdownMenuItem<RecordType>(
-                                  value: type,
-                                  child: Text(type.name),
-                                );
-                              }).toList(),
-                            ),
-                          ],
-                        ),
+                ListView.builder(
+                  scrollDirection: Axis.vertical,
+                  shrinkWrap: true,
+                  itemCount: state.items.length,
+                  itemBuilder: (_, int index) {
+                    return DismissibleCard(
+                      // 最後の一つは消せないようにする
+                      isDismissible: event.items.length != 1,
+                      onDismissed: (_) {
+                        event.removeItem(index);
+                      },
+                      child: Column(
+                        children: [
+                          TextFormField(
+                            maxLength: 20,
+                            validator: emptyValidator,
+                            decoration: InputDecoration(labelText: '表示名'),
+                            initialValue: state.items[index].name,
+                            onChanged: (v) {
+                              event.updateItemName(index, v);
+                            },
+                          ),
+                          DropdownButtonFormField<RecordType>(
+                            decoration: InputDecoration(labelText: 'レコードタイプ'),
+                            icon: Icon(Icons.arrow_downward),
+                            iconSize: 20,
+                            value: state.items[index].type,
+                            onChanged: (v) {
+                              event.updateItemType(index, v);
+                            },
+                            items: RecordType.values
+                                .where((e) => e != RecordType.NONE)
+                                .map<DropdownMenuItem<RecordType>>(
+                                    (RecordType type) {
+                              return DropdownMenuItem<RecordType>(
+                                value: type,
+                                child: Text(type.name),
+                              );
+                            }).toList(),
+                          ),
+                        ],
                       ),
-                    ),
-                    onDismissed: (_) {
-                      event.removeItem(index);
-                    },
-                  );
-                },
-              ),
-              Container(
-                padding: EdgeInsets.all(10),
-                child: ElevatedButton.icon(
-                  onPressed: () {
-                    event.addItem();
+                    );
                   },
-                  icon: Icon(Icons.add, size: 18),
-                  label: Text("追加"),
                 ),
-              ),
-            ],
+                if (state.items.length != 10)
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    child: ElevatedButton.icon(
+                      onPressed: () {
+                        event.addItem();
+                      },
+                      icon: Icon(Icons.add, size: 18),
+                      label: Text('追加'),
+                    ),
+                  )
+              ],
+            ),
           ),
         ),
       ),
