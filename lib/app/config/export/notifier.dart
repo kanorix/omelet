8b134221acf0887059e33f7a0f3b324d01ea2a43
u9uru <u9uru@icloud.com>
2021-03-17 04:03:12 +0900

    [create] 仮画面作成

--- lib/app/config/export/notifier.dart
+++ lib/app/config/export/notifier.dart
@@ -4,3 +4,4 @@ export 'package:omelet/app/notifier/template/template_list_notifier.dart';
 export 'package:omelet/app/notifier/template/template_create_notifier.dart';
 
 export 'package:omelet/app/notifier/record/record_list_notifier.dart';
+export 'package:omelet/app/notifier/record/record_create_notifier.dart';
